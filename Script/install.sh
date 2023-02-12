b62fb88f830a9411b8598abcf8591499920d0097
systechae <85930518+systechae@users.noreply.github.com>
2023-02-12 16:52:56 +0400

    Update install.sh

--- Script/install.sh
+++ Script/install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-hello_world () {
-   echo 'hello, world'
+check_package () {
+   rpm -qa |grep "$1"
+   status=$?
+   [ $status -eq 0 ] && echo "command successful" || echo "command unsuccessful"
 }
 
 for package in `cat ./Packages/package.list`
 do
    echo "Package : $package"
+   check_package "$package"
 done
 
