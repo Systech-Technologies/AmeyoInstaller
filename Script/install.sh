502feb75ff27108ca4982b62723a0bf328559adc
systechae <85930518+systechae@users.noreply.github.com>
2023-02-13 11:51:06 +0400

    changes

--- Script/install.sh
+++ Script/install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 check_package () {
-   rpm -qa |grep "$1"
+   rpm -qa |grep "$1" > ./Packages/package.tmp
    status=$?
    if [[ $status -eq 0 ]];
    then
-      echo "command successful"
+      grep "$1" ./Packages/package.version >> ./Packages/package.tmp
+      latest_version =  `cat ./Packages/package.tmp |sort |-r |head`
+      if [[ $1 == *$latest_version* ]]; then
+         echo "Package: $1 | version is up to date"
+      else
+         echo "Package: $1 | has an updated version"
+      fi
    else
       echo "command unsuccessful"
    fi
 }
+
+echo "Generating the Latest Package List"
+sshpass -p "ftea.com" ssh -p 2242 haseebkc@ccu.systech.ae -q -o "StrictHostKeyChecking no" "cd /dacx/Ameyo_package;ls" > ./Packages/package.version
 
 for package in `cat ./Packages/package.list`
 do
